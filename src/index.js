b82524081ffad62dd52ed8e2bf70dcdb0ecaa24a
basselhilf <basselhilf@gmail.com>
2020-12-25 23:11:55 +0200

    WebPack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import lodash from 'lodash';
+import Renderer from './Renderer'
+import APIManager from './APIManager'
+import './style.css'
+
+
+const renderer = new Renderer()
+const apiManager = new APIManager()
+
+const loadData = () => apiManager.loadData()
+const renderData = () => renderer.render(apiManager.data)
+
+$('#load-btn').on('click', loadData)
+$('#display-btn').on('click', renderData)
